04093849c7bb64a905e2605b83d08b860f211b9c
Aaditya Menon <addy.menon@outlook.com>
2016-07-09 21:27:07 +0530

    Minor Changes

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 app.controller('MainController', function($scope, $http) {
   $scope.title = 'Welcome to the Shop';
   $scope.promo = "Like Your Retail Store";
-  $http.get("customers.php").then(function (response) {
+  $http.get("products.php").then(function (response) {
       $scope.products = response.data.products;
   });
 });
