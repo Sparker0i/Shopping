30bc9d9af8e9559bb21d249144bd5dc68e5236ea
Aaditya Menon <addy.menon@outlook.com>
2016-07-09 20:48:54 +0530

    Products Synced with JSON

---
+++ customers.php
@@ -0,0 +1,58 @@
+{
+  "products" : [
+    {
+      "name": "Moto G3",
+      "price": 179.99,
+      "arrival": "2015-06-29",
+      "cover": "img/Moto G3.jpg"
+    },
+    {
+      "name": "Lumia 640XL",
+      "price": 229.99,
+      "arrival": "2015-09-06",
+      "cover": "img/Lumia 640XL.jpg"
+    },
+    {
+      "name": "Moto X Style",
+      "price": 383.99,
+      "arrival": "2015-06-28",
+      "cover": "img/Moto X Style.jpg"
+    },
+    {
+      "name": "Nexus 5X",
+      "price": 429.99,
+      "arrival": "2015-08-29",
+      "cover": "img/Nexus-5X.jpg"
+    },
+    {
+      "name": "Yu Yunicorn",
+      "price": 194.99,
+      "arrival": "2016-06-07",
+      "cover": "img/Yu Yunicorn.jpg"
+    },
+    {
+      "name": "iPhone 6S",
+      "price": 1138.49,
+      "arrival": "2015-09-13",
+      "cover": "img/iPhone6S.jpg"
+    },
+    {
+      "name": "Zenfone 2",
+      "price": 384.99,
+      "arrival": "2015-04-01",
+      "cover": "img/Zenfone 2.jpg"
+    },
+    {
+      "name": "Redmi Note 3",
+      "price": 179.99,
+      "arrival": "2016-01-11",
+      "cover": "img/Redmi Note 3.jpg"
+    },
+    {
+      "name": "LeEco Le2",
+      "price": 179.99,
+      "arrival": "2016-02-10",
+      "cover": "img/LeEco Le2.jpg"
+    }
+  ]
+}
